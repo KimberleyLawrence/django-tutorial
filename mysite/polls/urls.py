279f3cacce14d13be30fbb038aad2033f2759abb
KimberleyLawrence <kimberleynedic@gmail.com>
2016-01-20 20:26:49 -0800

    Creating polls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 urlpatterns =[
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    
 ]
 # ^$ means start^ of the url and $end of the url, so pretty much means empty
